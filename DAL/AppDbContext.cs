50471ba3eb1bc4c322e50d1cd94e46fe0295a482
Celinarabe <41392379+Celinarabe@users.noreply.github.com>
2018-11-18 14:22:53 -0600

    Add files via upload

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Rabe_Celina_HW6.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Rabe_Celina_HW6.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        //constructor that invokes the base constructor
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+
+        //create the db set
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<SupplierDetail> SupplierDetails { get; set; }
+
+    }
+}
+
